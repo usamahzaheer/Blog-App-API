ee1ca482744e188b6ba2a1fa9665f50317f3daab
usamahzaheer <sardarosamahkhan@gmail.com>
2021-02-17 01:31:28 +0500

    backend completed

--- app.js
+++ app.js
@@ -1,24 +1,63 @@
 const express = require("express");
 const app = express();
-const Post = require("./api/models/posts");
-const postData = new Post();
+const multer  = require('multer');
+const Post = require('./api/models/posts');
+const postsData = new Post();
 
-app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","*");
-    next();
-});
-app.use('/uploads',express.static('uploads'));
-
-app.get("/api/posts",(req, res)=>{
-    res.status(200).send(postData.get());
-});
-app.get("/api/posts/:post_id",(req, res)=>{
-    const postId = req.params.post_id;
-    const foundPost = postData.getIndividualBlog(postId);
-    if( foundPost ){
-        res.status(200).send(foundPost)
-    } else{
-        res.status(404).send("Not Found");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`)
     }
 });
-app.listen(3000, ()=>console.log("listening on http://localhost:3000"));
+
+const getExt = (mimetype) => {
+    switch(mimetype){
+        case "image/png":
+            return '.png';
+        case "image/jpeg":
+            return '.jpg';
+    }
+}
+
+var upload = multer({ storage: storage });
+
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+app.get("/api/posts", (req, res)=>{
+    res.status(200).send(postsData.get());
+})
+
+app.get("/api/posts/:postId", (req, res)=>{
+    const postId = req.params.postId;
+    const posts = postsData.get();
+    const foundPost = posts.find((post) => post.id == postId);
+    if(foundPost){
+        res.status(200).send(foundPost);
+    } else {
+        res.status(404).send("Not Found")
+    }
+})
+
+app.post("/api/posts", upload.single("post-image") ,(req, res)=>{
+    
+    const newPost = {
+        "id": `${Date.now()}`,
+        "title": req.body.title,
+        "content": req.body.content,
+        "post_image": req.file.path.replace("\\","/"),
+        "added_date": `${Date.now()}`
+    }
+    postsData.add(newPost);
+    res.status(201).send(req.file.path);
+})
+
+app.listen(3000, ()=>console.log("Listening on http://localhost:3000/"));
